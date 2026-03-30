de1c22c749773e83d83c7b426ea14ce14346cacd
pedro.plopes@sempreceub.com <peedrosetubal9@gmail.com>
2026-03-30 17:10:23 -0300

    x

--- core/views.py
+++ core/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from properties.models import Property
 
-
 def home(request):
-    destaques = Property.objects.filter(ativo=True).order_by("-criado_em")[:8]
-    return render(request, "core/home.html", {"destaques": destaques})
+    destaques = Property.objects.filter(destaque=True, ativo=True)[:6]
+    return render(request, 'core/home.html', {'destaques': destaques})
--- properties/admin.py
+++ properties/admin.py
@@ -1,29 +1,5 @@
-from django import forms
 from django.contrib import admin
-from django.core.exceptions import ValidationError
 from .models import PropertyType, Property, PropertyImage
-
-# Tamanho máximo de upload (5 MB)
-MAX_IMAGE_SIZE_MB = 5
-MAX_IMAGE_BYTES = MAX_IMAGE_SIZE_MB * 1024 * 1024
-
-
-class PropertyImageForm(forms.ModelForm):
-    class Meta:
-        model = PropertyImage
-        fields = "__all__"  
-
-    def clean_imagem(self):
-        arquivo = self.cleaned_data.get("imagem")
-        if arquivo and hasattr(arquivo, "size") and arquivo.size > MAX_IMAGE_BYTES:
-            raise ValidationError(f"Imagem não pode ter mais de {MAX_IMAGE_SIZE_MB} MB.")
-        return arquivo
-
-
-class PropertyImageInline(admin.TabularInline):
-    model = PropertyImage
-    form = PropertyImageForm
-    extra = 1
 
 
 @admin.register(PropertyType)
@@ -38,7 +14,6 @@ class PropertyAdmin(admin.ModelAdmin):
     search_fields = ("titulo", "endereco", "cidade")
     list_editable = ("ativo", "destaque")
     prepopulated_fields = {"slug": ("titulo",)}
-    inlines = [PropertyImageInline]
     date_hierarchy = "criado_em"
 
 
